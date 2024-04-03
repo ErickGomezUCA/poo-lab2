a85143e385aa49a9398ecc88b257620cf2b23754
ErickGomezUCA <00300723@uca.edu.sv>
2024-04-03 16:31:15 -0600

    Implementar funciones de Library en Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Library myLibrary = new Library();
+
+        myLibrary.addBook(new Book(20334993, "Dracula", "Bram Stroker", 101));
+        myLibrary.addBook(new Book(467854, "Cien años de soledad", "Gabriel Marquez", 204));
+        myLibrary.addBook(new Book("Libro 1", "Author 1", 131));
+        myLibrary.addBook(new Book("Libro 2", "Author 2", 646));
+
+        myLibrary.showBooks();
     }
 }
